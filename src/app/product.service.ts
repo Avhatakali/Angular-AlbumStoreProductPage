054c2981a1c5b3851a32db1aa846c9df45060509
Avhatakali Makwarela <avhatakali@Avhatakalis-iMac.local>
2019-10-29 13:06:00 +0200

    Album added

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Album } from './album';
+import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
@@ -13,8 +15,8 @@ export class ProductService {
    * method
    * getAlbum 
    */
-  public getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response) => response.json());
+  public getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
 }
